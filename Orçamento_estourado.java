969358f9be6526d984cec9930a822f592ff3685f
WillyanCodes <willyanrossanelliwress2703@gmail.com>
2024-09-18 17:17:04 -0300

    Orçamento_estourado.java
    Este projeto é muito simples, não muito útil. Você deve colocar o teu salário mensal, ou o tanto que você tem atualmente e colocará o preço da sua dívida, onde mostrara se você tem o dinheiro suficiente para pagar a dívida ou não.

---
+++ Orçamento_estourado.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Qual é o seu sálario? ");
+        
+        float n1 = scan.nextFloat();
+        
+        System.out.println("Qual foi o valor gasto? ");
+        float n2 = scan.nextFloat();
+        
+        
+        if (n1>=n2) {
+            System.out.println("Gastos dentro do orçamento.");
+            
+        } else {
+            System.out.println("Orçamento estourado.");
+        
+}
+
+        
+   
+   
+   
+   
+   
+   
+    }
+}

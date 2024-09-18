7394f0252e7047d5a8914e7d9e5281e17fe72e05
WillyanCodes <willyanrossanelliwress2703@gmail.com>
2024-09-18 17:18:24 -0300

    É o mesmo número.java
    Você devera digitar 2 números, aonde mostrara se são os mesmos ou não.

---
+++ É o mesmo número.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Digite o 1 número: ");
+        
+        float n1 = scan.nextFloat();
+        
+        System.out.println("Digite o 2 número: ");
+        float n2 = scan.nextFloat();
+        
+        
+        if (n1==n2) {
+            System.out.println("É o mesmo número.");
+            
+        } else {
+            System.out.println("Não é o mesmo número.");
+        
+}
+
+        
+   
+   
+   
+   
+   
+   
+    }
+}

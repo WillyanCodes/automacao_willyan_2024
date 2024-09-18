736dccb4ba9b4b696ff6bd2f6831c52775a56557
WillyanCodes <willyanrossanelliwress2703@gmail.com>
2024-09-18 17:23:22 -0300

    Você é maior de idade.java
    Você é maior de idade?
    
    
    
    
    
    
    Se não, aproveite kkkkk

---
+++ Você é maior de idade.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Digite a sua idade: ");
+        int idade = scan.nextInt();
+        
+        if (idade>18) {
+            System.out.println("Você é maior de idade. ");
+            
+        } else {
+            System.out.println("Você é menor de idade. ");
+        
+}
+
+        
+   
+   
+   
+   
+   
+   
+    }
+}
